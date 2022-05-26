b42c40d5037ee7b7d18f56473952304476d2f532
Tiago Carmo Santos <tiagocarmosantos@hotmail.com>
2022-05-26 07:38:23 -0300

    ci(ci): add trackers to ci-golang-workflow yaml file

--- frameworks/node/loader.js
+++ frameworks/node/loader.js
@@ -1,6 +1,6 @@
-import server from './config/server.js'
-import logger from './config/logger.js'
-import openedRouter from './config/openedRouter.js'
+import server from './config/server.js';
+import logger from './config/logger.js';
+import openedRouter from './config/openedRouter.js';
 
 logger(server);
 openedRouter(server);
